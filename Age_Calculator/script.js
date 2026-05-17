3ed4554197b27fd8cde1a31705435e0bbfa889b4
Joe Ferrer <ferrerjosephsubia@gmail.com>
2026-05-17 21:45:26 +0800

    Got a bit busy from school, but i finished the function of the age calculator, gonna start a new project real soon!

---
+++ Age_Calculator/script.js
@@ -0,0 +1,58 @@
+const button = document.getElementById("button");
+
+const date = new Date();
+
+button.onclick = function(){
+
+    let birth = document.getElementById("date").value;
+
+    birth = birth.split("-");
+
+    const birth_year = birth[0];   
+    const birth_month = birth[1];
+    const birth_day = birth[2];
+
+    console.log("--BIRTHDATE--");
+    console.log(birth_year);
+    console.log(birth_month);
+    console.log(birth_day);
+    
+    const current_year = date.getFullYear();
+    const current_month = date.getMonth();
+    const current_day = date.getDate();
+
+    console.log("--CURRENTDATE--");
+    console.log(current_year);
+    console.log(current_month);
+    console.log(current_day);
+
+    let age = current_year - birth_year;
+
+    console.log("--AGEDATE--");
+
+    if(birth_month > current_month && birth_day > current_day){
+
+        age -= 1;
+
+        console.log(`My age is ${age}`);
+        document.getElementById("age").textContent = age;
+
+
+    }
+    else{
+
+        console.log(`My age is ${age}`);
+        document.getElementById("age").textContent = age;
+
+    }
+
+
+    
+
+
+
+
+
+
+
+}
