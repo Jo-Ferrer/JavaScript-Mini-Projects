fc081ed18719afbbfea1127063eb9cb440b4f1ee
Joe Ferrer <ferrerjosephsubia@gmail.com>
2026-06-04 21:29:28 +0800

    Been busy since its already finals I figure out the function of the dice, and currently added a few design, still at the design

--- Dice_Roll_Simulator/script.js
+++ Dice_Roll_Simulator/script.js
@@ -1,17 +1,7 @@
 const max = 6;
 const min = 1;
-const dice = 3;
-let i = 1;
 
-
-
-for(i; i <= dice; i++){
-
-
-    let guess = Math.floor(Math.random() * (max - min + 1)) + min;
+let guess = Math.floor(Math.random() * (max - min + 1)) + min;
     console.log(guess);
-
-}
-
 
 
